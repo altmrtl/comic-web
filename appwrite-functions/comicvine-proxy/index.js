5490f2c4caa38ac789aa9b648116f6aaa4ba30fc
seikohanma <pacvieirah@gmail.com>
2025-08-15 11:48:12 +0100

    appwrite function folder

---
+++ appwrite-functions/comicvine-proxy/index.js
@@ -0,0 +1,16 @@
+import fetch from "node-fetch";
+
+export default async ({ req, res, env }) => {
+    const search = req.query.q || "batman";
+    const API_KEY = env.VITE_COMIC_API_KEY;
+
+    const url = `https://comicvine.gamespot.com/api/search/?api_key=${API_KEY}&format=json&query=${encodeURIComponent(search)}&resources=volume`;
+
+    try {
+        const response = await fetch(url);
+        const data = await response.json();
+        return res.json(data);
+    } catch (err) {
+        return res.json({ error: err.message }, 500);
+    }
+};
